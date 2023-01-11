7af68eaf4d33d4c3524db108b9f5b2bdae7a2328
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-01-11 14:43:07 +0100

    Add build.sh
    This is a first version of the ci
    
    The tests launches are just a cukinia file
    The repository are mine for test purpose

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+GITHUB_REF=$1
+CI_DIR=`pwd`
+PR_N=`echo $GITHUB_REF | cut -d '/' -f 3`
+TIME=`date +%Hh%Mm%S`
+
+# Get sources
+git clone git@github.com:eroussy/ansible.git
+cd ansible
+git fetch origin ${GITHUB_REF}
+git checkout FETCH_HEAD
+
+# Launch tests
+mkdir $CI_DIR/cukinia_tests
+cukinia -f junitxml -o $CI_DIR/cukinia_tests/cukinia.xml $CI_DIR/ansible/cukinia.conf || true
+
+# Create report
+REPORT=test-report_pr-${PR_N}_${TIME}.pdf
+cd $CI_DIR/ci/report-generator
+cqfd init
+CQFD_EXTRA_RUN_ARGS="-v ${CI_DIR}/cukinia_tests:/tmp/cukinia-res" cqfd run
+
+# Upload report
+cd $CI_DIR
+REPORT_PR_DIR=PR-${PR_N}
+git clone --depth 1 -b site git@github.com:eroussy/ci-seapath.git $CI_DIR/site
+cd $CI_DIR/site
+mkdir -p docs/reports/$REPORT_PR_DIR
+mv $CI_DIR/ci/report-generator/main.pdf docs/reports/${REPORT_PR_DIR}/${REPORT}
+git add docs/reports/${REPORT_PR_DIR}/${REPORT}
+git commit -m "upload report $REPORT"
+git push origin site
+
+# Give link
+echo See test Report at \
+https://eroussy.github.io/ci-seapath/reports/${REPORT_PR_DIR}/${REPORT}
+
+# grep for succes
+if grep -q "<failure" $CI_DIR/cukinia_tests/*; then
+  RES=1
+else
+  RES=0
+fi
+
+# remove github clone dir and cukinia test dir
+rm -rf $CI_DIR
+exit $RES
