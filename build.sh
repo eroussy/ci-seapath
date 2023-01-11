17e814c0e64225568e49132df0484b3b2ed3659d
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-01-11 16:51:50 +0100

    Update script for fully automated ci

--- build.sh
+++ build.sh
@@ -1,53 +1,54 @@
 #!/bin/bash
 
-GITHUB_REF=$1
-CI_DIR=/home/eroussy/Documents/RTE/ci-seapath
+# CI_DIR=/tmp/seapath-ci-${RANDOM}
+# mkdir $CI_DIR
+# cd $CI_DIR
+# git clone --depth 1 -b test-perso git@github.com:eroussy/ci-seapath.git ci
+# ci/build.sh
 
+GITHUB_REF=$1
+CI_DIR=`pwd`
 PR_N=`echo $GITHUB_REF | cut -d '/' -f 3`
 TIME=`date +%Hh%Mm%S`
 
 # Get sources
-CLONE_DIR=/tmp/ansible_PR-${PR_N}_${TIME}
-git clone git@github.com:eroussy/ansible.git $CLONE_DIR
-cd $CLONE_DIR
+git clone git@github.com:eroussy/ansible.git
+cd ansible
 git fetch origin ${GITHUB_REF}
 git checkout FETCH_HEAD
 
-
 # Launch tests
-CUKINIA_TEST_DIR=/tmp/cukinia-test_PR-${PR_N}_${TIME}
-mkdir $CUKINIA_TEST_DIR
-cukinia -f junitxml -o $CUKINIA_TEST_DIR/cukinia.xml $CLONE_DIR/cukinia.conf
-
+mkdir $CI_DIR/cukinia_tests
+cukinia -f junitxml -o $CI_DIR/cukinia_tests/cukinia.xml $CI_DIR/ansible/cukinia.conf
 
 # Create report
 REPORT=test-report_pr-${PR_N}_${TIME}.pdf
-cd $CI_DIR/report-generator
-CQFD_EXTRA_RUN_ARGS="-v ${CUKINIA_TEST_DIR}:/tmp/cukinia-res" cqfd run
-cd ..
-
+cd $CI_DIR/ci/report-generator
+CQFD_EXTRA_RUN_ARGS="-v ${CI_DIR}/cukinia_tests:/tmp/cukinia-res" cqfd run
 
 # Upload report
+cd $CI_DIR
 REPORT_PR_DIR=PR-${PR_N}
+git clone --depth 1 -b site git@github.com:eroussy/ci-seapath.git $CI_DIR/site
+cd $CI_DIR/site
 mkdir -p docs/reports/$REPORT_PR_DIR
-mv report-generator/main.pdf docs/reports/${REPORT_PR_DIR}/${REPORT}
-# TODO : what if local changes
-git switch site
+mv $CI_DIR/ci/report-generator/main.pdf docs/reports/${REPORT_PR_DIR}/${REPORT}
+
 git add docs/reports/${REPORT_PR_DIR}/${REPORT}
 git commit -m "upload report $REPORT"
 git push origin site
+
 gh api --method POST   -H "Accept: application/vnd.github+json" \
 /repos/eroussy/ansible/pulls/${PR_N}/reviews -f event='COMMENT' \
 -f body="Report available at \
 https://eroussy.github.io/ci-seapath/reports/${REPORT_PR_DIR}/${REPORT}"
-git switch main
 
 # grep for succes
-if grep -q "<failure" $CUKINIA_TEST_DIR/*; then
+if grep -q "<failure" $CI_DIR/cukinia_tests/*; then
   exit 1
 else
   exit 0
 fi
 
 # remove github clone dir and cukinia test dir
-rm -rf $CLONE_DIR $CUKINIA_TEST_DIR
+rm -rf $CI_DIR
