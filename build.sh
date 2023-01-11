3b89019cab867fad70dd0c4a68f107b7f59812aa
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-01-11 15:44:49 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,42 +1,43 @@
-GITHUB_REF=refs/pull/10/merge
+GITHUB_REF=refs/pull/1/merge
+CI_DIR=/home/eroussy/Documents/RTE/ci-seapath
 
 PR_N=`echo $GITHUB_REF | cut -d '/' -f 3`
 TIME=`date +%Hh%Mm%S`
 
 # Get sources
-CLONE_DIR=/tmp/gha-test_PR-${PR_N}_${TIME}
-git clone git@github.com:eroussy/gha-test.git $CLONE_DIR
+CLONE_DIR=/tmp/ansible_PR-${PR_N}_${TIME}
+git clone git@github.com:eroussy/ansible.git $CLONE_DIR
 cd $CLONE_DIR
 git fetch origin ${GITHUB_REF}
 git checkout FETCH_HEAD
 
 
 # Launch tests
-
 CUKINIA_TEST_DIR=/tmp/cukinia-test_PR-${PR_N}_${TIME}
 mkdir $CUKINIA_TEST_DIR
-cukinia -f junitxml -o $CUKINIA_TEST_DIR/cukinia.xml
+cukinia -f junitxml -o $CUKINIA_TEST_DIR/cukinia.xml $CLONE_DIR/cukinia.conf
 
 
 # Create report
-
 REPORT=test-report_pr-${PR_N}_${TIME}.pdf
-#  TODO cd /home/eroussy/Documents/RTE/CI/report-generator
+cd $CI_DIR/report-generator
 CQFD_EXTRA_RUN_ARGS="-v ${CUKINIA_TEST_DIR}:/tmp/cukinia-res" cqfd run
 cd ..
 
 
 # Upload report
-
 REPORT_PR_DIR=PR-${PR_N}
 mkdir -p docs/reports/$REPORT_PR_DIR
 mv report-generator/main.pdf docs/reports/${REPORT_PR_DIR}/${REPORT}
+git switch site
 git add docs/reports/${REPORT_PR_DIR}/${REPORT}
 git commit -m "upload report $REPORT"
 git push origin site
 gh api --method POST   -H "Accept: application/vnd.github+json" \
-/repos/eroussy/gha-test/pulls/${PR_N}/reviews -f event='COMMENT' \
--f body='Report available at https://eroussy.github.io/gha-test/reports/${REPORT}'
+/repos/eroussy/ansible/pulls/${PR_N}/reviews -f event='COMMENT' \
+-f body="Report available at \
+https://eroussy.github.io/ci-seapath/reports/${REPORT_PR_DIR}/${REPORT}"
+git switch main
 
 # grep for succes
 if grep -q "<failure" $CUKINIA_TEST_DIR/*; then
@@ -46,5 +47,4 @@ else
 fi
 
 # remove github clone dir and cukinia test dir
-
 rm -rf $CLONE_DIR $CUKINIA_TEST_DIR
