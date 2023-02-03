6e4eea0457283fd29b14967656acac322914ce5e
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-02-03 15:32:10 +0100

    launch.sh: correct shellcheck errors
    Correct all the warnings raised by shellcheck
    
    Signed-off-by: Erwann Roussy <erwann.roussy@savoirfairelinux.com>

--- launch.sh
+++ launch.sh
@@ -8,7 +8,7 @@
 set -e
 
 die() {
-	echo "CI internal failure : $@" 1>&2
+	echo "CI internal failure : $*" 1>&2
 	exit 1
 }
 
@@ -41,6 +41,7 @@ initialization() {
   cqfd init
   cqfd -b prepare
   echo "Sources prepared succesfully"
+  exit 0
 }
 
 # Launch Debian configuration and hardening
@@ -55,6 +56,7 @@ configure_debian() {
   playbooks/cluster_setup_debian.yaml \
   playbooks/cluster_setup_hardened_debian.yaml
   echo "Debian set up succesfully"
+  exit 0
 }
 
 # Prepare, launch test and upload test report
@@ -82,8 +84,8 @@ launch_test() {
 
   # Upload report
 
-  PR_N=`echo $GITHUB_REF | cut -d '/' -f 3`
-  TIME=`date +%F_%Hh%Mm%S`
+  PR_N=$(echo $GITHUB_REF | cut -d '/' -f 3)
+  TIME=$(date +%F_%Hh%Mm%S)
   REPORT_NAME=test-report_${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}_${TIME}.pdf
   REPORT_DIR=${WORK_DIR}/reports/docs/reports/PR-${PR_N}
 
@@ -116,15 +118,12 @@ launch_test() {
 case "$1" in
   init)
     initialization
-    exit 0
     ;;
   conf)
     configure_debian
-    exit 0
     ;;
   test)
     launch_test
-    exit 0
     ;;
   *)
     usage
