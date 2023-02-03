ecd04b483aa41eff86f01f7c223936f21357adcc
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-02-03 15:37:55 +0100

    wip

--- report-generator/compile.sh
+++ report-generator/compile.sh
@@ -88,10 +88,11 @@ add_xml_to_adoc()
     local nb_suites="$(( $(xmlstarlet sel -t -v  '//testsuite/@name' "$xml"\
         |wc -l) +1 ))"
     for i in $(seq 1 ${nb_suites}) ; do
-        local testname=$(xmlstarlet sel -t -v "//testsuite[$i]/@name" "$xml")
-        local classname=$(xmlstarlet sel -t -v "//testsuite[$i]/testcase[1]/@classname" "$xml")
-        local nb_tests=$(xmlstarlet sel -t -v "//testsuite[$i]/@tests" "$xml")
-        local failures=$(xmlstarlet sel -t -v "//testsuite[$i]/@failures" "$xml")
+        local testname classname nb_tests failures
+        testname=$(xmlstarlet sel -t -v "//testsuite[$i]/@name" "$xml")
+        classname=$(xmlstarlet sel -t -v "//testsuite[$i]/testcase[1]/@classname" "$xml")
+        nb_tests=$(xmlstarlet sel -t -v "//testsuite[$i]/@tests" "$xml")
+        failures=$(xmlstarlet sel -t -v "//testsuite[$i]/@failures" "$xml")
         local cols='"7,1"'
         if [ -z "$testname" -o "$testname" == "default" ] ; then
             testname=$(basename $xml)
